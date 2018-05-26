63fb824ec112289f79ea13deba8a7f9c52ba322e
MoisesLopezp <moicano324@hotmail.com>
2018-05-26 00:40:20 -0500

    Tipos de disparo

--- Assets/CosasMoy/Scripts/scr_Gun.cs
+++ Assets/CosasMoy/Scripts/scr_Gun.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scr_Gun : MonoBehaviour {
 
     public GameObject Cannon;
     public LayerMask MaskShoot;
     public GameObject HitEffect;
+
+    public Text T_TypeW;
 
     float CD = 0f; 
 
@@ -21,6 +24,22 @@ public class scr_Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetButtonDown("NextW"))
+        {
+            TypeShoot++;
+            if (TypeShoot > 3)
+                TypeShoot = 0;
+            T_TypeW.text = TypeShoot.ToString();
+        }
+
+        if (Input.GetButtonDown("PrevW"))
+        {
+            TypeShoot--;
+            if (TypeShoot < 0)
+                TypeShoot = 3;
+            T_TypeW.text = TypeShoot.ToString();
+        }
+
         if (CD > 0f)
             CD -= Time.deltaTime;
 
@@ -42,6 +61,7 @@ public class scr_Gun : MonoBehaviour {
                     GameObject hitef = Instantiate(HitEffect, hit.point, Quaternion.identity);
                     Destroy(hitef, 0.5f);
                 }
+                CD = 0.5f;
             }
         }
 
