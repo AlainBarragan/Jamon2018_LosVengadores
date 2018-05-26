f565dac7b256b947e475ba549818b35e870cd841
Alain <barraganalain55@gmail.com>
2018-05-26 17:07:46 -0500

    Ya arregle lo del chamuco

---
+++ Assets/Cosas De Alain/Scripts/SCR_Misil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Misil : MonoBehaviour 
+{
+    Transform player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<scr_Player>().transform;
+    }
+
+    private void Update()
+    {
+        this.transform.LookAt(player);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, player.position, 10 * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.CompareTag("Player"))
+        {
+
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Cosas De Alain/Scripts/SCR_Torreta.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Torreta : MonoBehaviour 
+{
+    public GameObject misil;
+
+    bool disparar;
+    float timer;
+    Transform player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<scr_Player>().transform;
+        timer = 0;
+        disparar = false;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < 50)
+            disparar = true;
+        else
+            disparar = false;
+
+        if (disparar)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 10)
+            {
+                timer = 0;
+                Instantiate(misil, this.transform);
+            }
+        }
+    }
+}
