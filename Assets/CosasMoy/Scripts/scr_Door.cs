6f0eb7bc3aab6b376c16a24c2c0e3b8ce6045a00
MoisesLopezp <moicano324@hotmail.com>
2018-05-26 21:38:55 -0500

    Dron Texturizado Y puerta

---
+++ Assets/CosasMoy/Scripts/scr_Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Door : MonoBehaviour {
+
+    Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Anim.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Anim.SetBool("Open", false);
+        }
+    }
+}
